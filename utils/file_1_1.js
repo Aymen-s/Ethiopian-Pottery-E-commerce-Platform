bad130dc2e656072a6b4a357feef8251fc0dd2c7
Aymen-s <aymensileshi77@gmail.com>
2024-08-14 19:46:00 +0300

    fix: correct cart quantity calculation

--- utils/file_1_1.js
+++ utils/file_1_1.js
@@ -15,3 +15,4 @@ function module11() {
 module.exports = module11;
 // Update: Commit 2 on 2024-08-12 at 14:22
 // Update: Commit 5 on 2024-08-13 at 10:54
+// Update: Commit 1 on 2024-08-14 at 19:46
