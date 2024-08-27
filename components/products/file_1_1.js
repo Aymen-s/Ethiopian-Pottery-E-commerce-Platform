36311c4d21058ac1fe8c2f4381867df0973e9881
Aymen-s <aymensileshi77@gmail.com>
2024-08-27 16:14:00 +0300

    refactor: restructure API service layer

--- components/products/file_1_1.js
+++ components/products/file_1_1.js
@@ -16,3 +16,4 @@ module.exports = module11;
 // Update: Commit 3 on 2024-08-13 at 11:7
 // Update: Commit 1 on 2024-08-22 at 13:5
 // Update: Commit 6 on 2024-08-27 at 18:44
+// Update: Commit 8 on 2024-08-27 at 16:14
