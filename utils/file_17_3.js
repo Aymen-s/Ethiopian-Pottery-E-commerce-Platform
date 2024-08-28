98608ecae3431e63a3f6e7ebcfc75e9bc20eeec0
Aymen-s <aymensileshi77@gmail.com>
2024-08-28 13:59:00 +0300

    docs: update API documentation

--- utils/file_17_3.js
+++ utils/file_17_3.js
@@ -1,5 +1,5 @@
 /**
- * feat: add user authentication system
+ * fix: resolve image upload size limit
  * Created: 2024-08-28
  */
 
@@ -13,6 +13,3 @@ function module173() {
 }
 
 module.exports = module173;
-// Update: Commit 2 on 2024-10-17 at 19:31
-// Update: Commit 3 on 2024-12-12 at 15:58
-// Update: Commit 4 on 2024-12-17 at 12:6
