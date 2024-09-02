68e33346b85c7749d1d2cce0bcbca7adb69c27b2
Aymen-s <aymensileshi77@gmail.com>
2024-09-02 09:50:00 +0300

    feat: implement shopping cart functionality

--- components/cart/file_1_5.js
+++ components/cart/file_1_5.js
@@ -16,3 +16,4 @@ module.exports = module15;
 // Update: Commit 4 on 2024-08-13 at 19:38
 // Update: Commit 2 on 2024-08-14 at 10:39
 // Update: Commit 7 on 2024-08-21 at 14:37
+// Update: Commit 3 on 2024-09-02 at 9:50
