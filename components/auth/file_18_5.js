f740417d248890f463a7a58e1ee09f5043d23345
Aymen-s <aymensileshi77@gmail.com>
2024-09-12 09:24:00 +0300

    fix: correct cart quantity calculation

--- components/auth/file_18_5.js
+++ components/auth/file_18_5.js
@@ -15,3 +15,4 @@ function module185() {
 module.exports = module185;
 // Update: Commit 7 on 2024-09-02 at 19:57
 // Update: Commit 11 on 2024-09-03 at 14:25
+// Update: Commit 3 on 2024-09-12 at 9:24
