7476e853e77a3f3f6144688d827ab516ea522397
Aymen-s <aymensileshi77@gmail.com>
2024-09-16 16:10:00 +0300

    fix: resolve image upload size limit

--- services/file_9_5.js
+++ services/file_9_5.js
@@ -17,3 +17,4 @@ module.exports = module95;
 // Update: Commit 4 on 2024-08-27 at 10:22
 // Update: Commit 2 on 2024-09-02 at 16:16
 // Update: Commit 3 on 2024-09-02 at 17:47
+// Update: Commit 3 on 2024-09-16 at 16:10
