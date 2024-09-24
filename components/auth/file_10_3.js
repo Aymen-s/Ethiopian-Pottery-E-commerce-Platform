2083503547967f36e33eb17f66c4db8a5c0c0dd4
Aymen-s <aymensileshi77@gmail.com>
2024-09-24 19:54:00 +0300

    fix: resolve image upload size limit

--- components/auth/file_10_3.js
+++ components/auth/file_10_3.js
@@ -16,3 +16,4 @@ module.exports = module103;
 // Update: Commit 5 on 2024-08-21 at 10:19
 // Update: Commit 6 on 2024-08-21 at 16:37
 // Update: Commit 8 on 2024-09-03 at 13:51
+// Update: Commit 3 on 2024-09-24 at 19:54
