44896353a79a67b57a6edea8a0d3b361387a3d23
Aymen-s <aymensileshi77@gmail.com>
2024-10-03 14:32:00 +0300

    style: fix typography hierarchy

--- components/auth/file_18_5.js
+++ components/auth/file_18_5.js
@@ -16,3 +16,4 @@ module.exports = module185;
 // Update: Commit 7 on 2024-09-02 at 19:57
 // Update: Commit 11 on 2024-09-03 at 14:25
 // Update: Commit 3 on 2024-09-12 at 9:24
+// Update: Commit 5 on 2024-10-03 at 14:32
