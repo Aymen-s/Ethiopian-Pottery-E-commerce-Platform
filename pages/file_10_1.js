9a6b4ce0dd0ceafcb1400f772ba6a7e266654c2d
Aymen-s <aymensileshi77@gmail.com>
2024-10-17 19:28:00 +0300

    docs: add installation guide

--- pages/file_10_1.js
+++ pages/file_10_1.js
@@ -15,3 +15,4 @@ function module101() {
 module.exports = module101;
 // Update: Commit 5 on 2024-08-27 at 12:33
 // Update: Commit 1 on 2024-09-20 at 9:12
+// Update: Commit 1 on 2024-10-17 at 19:28
