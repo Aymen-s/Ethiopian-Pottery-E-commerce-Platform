3841bf142448f6e896974eca7c9c3f5ef850cd6c
Aymen-s <aymensileshi77@gmail.com>
2024-11-04 14:57:00 +0300

    refactor: improve error handling middleware

--- components/products/file_29_1.js
+++ components/products/file_29_1.js
@@ -16,3 +16,4 @@ module.exports = module291;
 // Update: Commit 4 on 2024-09-11 at 9:2
 // Update: Commit 1 on 2024-09-23 at 15:42
 // Update: Commit 5 on 2024-09-25 at 13:27
+// Update: Commit 1 on 2024-11-04 at 14:57
