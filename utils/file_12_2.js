13fd565839997b3f4657b0177812e19a6beefa9a
Aymen-s <aymensileshi77@gmail.com>
2024-12-03 18:40:00 +0300

    fix: correct price formatting display

--- utils/file_12_2.js
+++ utils/file_12_2.js
@@ -16,3 +16,4 @@ module.exports = module122;
 // Update: Commit 1 on 2024-09-13 at 19:6
 // Update: Commit 1 on 2024-10-13 at 17:0
 // Update: Commit 3 on 2024-10-15 at 10:22
+// Update: Commit 6 on 2024-12-03 at 18:40
