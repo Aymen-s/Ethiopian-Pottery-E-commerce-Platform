aa17c6f0b8eb804058420269939bb3d8c5a96a1c
Aymen-s <aymensileshi77@gmail.com>
2024-12-05 09:27:00 +0300

    docs: add installation guide

--- components/products/file_51_7.js
+++ components/products/file_51_7.js
@@ -15,3 +15,4 @@ function module517() {
 module.exports = module517;
 // Update: Commit 6 on 2024-10-09 at 16:7
 // Update: Commit 1 on 2024-10-25 at 13:37
+// Update: Commit 3 on 2024-12-05 at 9:27
