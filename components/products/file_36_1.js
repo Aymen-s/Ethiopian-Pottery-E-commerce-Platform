8f3fc12c9d3c3c5ecb9a1d988ade22a97143b2c3
Aymen-s <aymensileshi77@gmail.com>
2024-12-05 13:51:00 +0300

    fix: resolve login token expiration issue

--- components/products/file_36_1.js
+++ components/products/file_36_1.js
@@ -16,3 +16,4 @@ module.exports = module361;
 // Update: Commit 2 on 2024-09-26 at 9:14
 // Update: Commit 1 on 2024-11-08 at 18:27
 // Update: Commit 3 on 2024-11-18 at 11:49
+// Update: Commit 4 on 2024-12-05 at 13:51
