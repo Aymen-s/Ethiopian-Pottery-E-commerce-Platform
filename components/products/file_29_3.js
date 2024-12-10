87ab357d1ff23421aaa469323f95920a6a7e8b0e
Aymen-s <aymensileshi77@gmail.com>
2024-12-10 13:50:00 +0300

    docs: add installation guide

--- components/products/file_29_3.js
+++ components/products/file_29_3.js
@@ -15,3 +15,4 @@ function module293() {
 module.exports = module293;
 // Update: Commit 1 on 2024-09-10 at 19:9
 // Update: Commit 3 on 2024-12-06 at 12:37
+// Update: Commit 4 on 2024-12-10 at 13:50
