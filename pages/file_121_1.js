5a789608cdc53da24eb864f9811f1ba5d2cd2349
Aymen-s <aymensileshi77@gmail.com>
2024-12-10 17:19:00 +0300

    style: update button component styles

---
+++ pages/file_121_1.js
@@ -0,0 +1,15 @@
+/**
+ * fix: correct cart quantity calculation
+ * Created: 2024-12-10
+ */
+
+function module1211() {
+    // Implementation
+    return {
+        init: function() {
+            console.log('Module 121-1 initialized');
+        }
+    };
+}
+
+module.exports = module1211;
