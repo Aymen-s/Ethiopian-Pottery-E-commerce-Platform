525b1031370c00491f2884f3a2c0c83153ee8f66
Aymen-s <aymensileshi77@gmail.com>
2024-12-17 19:31:00 +0300

    fix: correct price formatting display

--- components/products/file_39_1.js
+++ components/products/file_39_1.js
@@ -15,3 +15,4 @@ function module391() {
 module.exports = module391;
 // Update: Commit 4 on 2024-09-19 at 12:54
 // Update: Commit 7 on 2024-11-12 at 15:29
+// Update: Commit 1 on 2024-12-17 at 19:31
