1602ec2e424c2c2a43817f4a7d61651ba69a6a61
Aymen <aymensileshi77@gmail.com>
2025-04-02 20:52:21 +0300

    completing the register user functionality from the backend

---
+++ backend/controllers/auth/auth-controller.js
@@ -0,0 +1,40 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../../models/User");
+
+const registerUser = async (req, res) => {
+  const { userName, email, password } = req.body;
+  const newUser = new User({
+    userName,
+    email,
+    password,
+  });
+  await newUser.save();
+  res.status(200).json({
+    success: true,
+    message: "Registration Successfull",
+  });
+
+  try {
+    const hashPassword = await bcrypt.hash(password, 12);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured!",
+    });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured!",
+    });
+  }
+};
+
+module.exports = { registerUser };
