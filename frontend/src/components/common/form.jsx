f1cf92d11d9d5c4c0ebc5b1a605d4c5c1dceb7dc
Aymen <aymensileshi77@gmail.com>
2025-04-05 14:37:20 +0300

    completing the add product component for the admin

--- frontend/src/components/common/form.jsx
+++ frontend/src/components/common/form.jsx
@@ -52,7 +52,7 @@ function CommonForm({
             value={value}
           >
             <SelectTrigger className="w-full">
-              <SelectValue placeholder={getControlItem.placeholder} />
+              <SelectValue placeholder={getControlItem.label} />
             </SelectTrigger>
             <SelectContent>
               {getControlItem.options && getControlItem.options.length > 0
--- frontend/src/pages/admin-view/products.jsx
+++ frontend/src/pages/admin-view/products.jsx
@@ -1,7 +1,69 @@
-import React from "react";
+import CommonForm from "@/components/common/form";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { addProductFormElements } from "@/config";
+import React, { useState } from "react";
+
+const initialFormData = {
+  image: null,
+  title: "",
+  desription: "",
+  category: "",
+  price: "",
+  brand: "",
+  salePrice: "",
+  totalStock: "",
+};
 
 function AdminProducts() {
-  return <div>AdminProducts</div>;
+  const [openCreateProductsDialog, setOpenCreateProductsDialog] =
+    useState(false);
+  const [formData, setFormData] = useState({ initialFormData });
+
+  function onSubmit() {
+    // Handle form submission
+    console.log(formData);
+  }
+  return (
+    <>
+      <div className="mb-5 flex justify-end w-full">
+        <Button
+          onClick={() => {
+            setOpenCreateProductsDialog(true);
+          }}
+        >
+          Add New product
+        </Button>
+      </div>
+      <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4"></div>
+      <Sheet
+        open={openCreateProductsDialog}
+        onOpenChange={() => {
+          setOpenCreateProductsDialog(false);
+        }}
+      >
+        <SheetContent side="right" className="overflow-auto">
+          <SheetHeader>
+            <SheetTitle>Add New Product</SheetTitle>
+          </SheetHeader>
+          <div className="py-6 px-4">
+            <CommonForm
+              formData={formData}
+              setFormData={setFormData}
+              formControls={addProductFormElements}
+              buttonText="Add"
+              onSubmit={onSubmit}
+            ></CommonForm>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </>
+  );
 }
 
 export default AdminProducts;
