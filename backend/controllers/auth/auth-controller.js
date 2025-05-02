e212f9b7f5a718c6daf11c1674234e83ce9d3d0b
Aymen <aymensileshi77@gmail.com>
2025-05-03 00:37:37 +0300

    changing cookie config

--- backend/controllers/auth/auth-controller.js
+++ backend/controllers/auth/auth-controller.js
@@ -68,7 +68,7 @@ const loginUser = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: true,
         sameSite: "strict",
       })
       .json({
